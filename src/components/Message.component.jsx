c4ba276ecba04a4c370943f45a714478f6b68fc6
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-09-26 16:48:30 +0700

    add message component

--- src/components/Message.component.jsx
+++ src/components/Message.component.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 import "./Message.styles.scss"
 
-export default function Message({ text }) {
+export default function Message({ text, rectanglePosition }) {
     return (
         <div className="message bg-white">
             {text}
+            <div className="rectangle" style={{
+                left: rectanglePosition === "left"
+                    ?   "15px" 
+                    : rectanglePosition === "right"
+                        ? `calc(100% - 45px)`
+                        : `calc(50% - 15px)` 
+            }}/>
         </div>
     )
 }
--- src/pages/Welcome.page.jsx
+++ src/pages/Welcome.page.jsx
@@ -10,7 +10,7 @@ export default class Welcome extends Component {
     render() {
         return (
             <div className="welcome-page">
-                <Message text={msg}/>
+                <Message text={msg} rectanglePosition="center"/>
             </div>
         )
     }
