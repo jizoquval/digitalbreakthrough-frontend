33f63f82cf630db18d1af99e9d0d7ff7057fd470
Val <grachev@sibext.com>
2020-09-26 16:45:15 +0700

    add mascot

--- src/pages/Welcome.page.jsx
+++ src/pages/Welcome.page.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import Message from '../components/Message.component'
+import React, { Component } from "react";
+import Mascot from "../components/Mascot";
+import Message from "../components/Message.component";
 
-import "./Welcome.styles.scss"
+import "./Welcome.styles.scss";
 
 const msg = `Привет! Я так рад видеть тебя! Давай дружить, я покажу самые крутые развлечения?
-Мы весело проведем этот день!`
+Мы весело проведем этот день!`;
 
 export default class Welcome extends Component {
-    render() {
-        return (
-            <div className="welcome-page">
-                <Message text={msg} rectanglePosition="center"/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="welcome-page">
+        <Message text={msg} />
+        <Mascot type={"mascotRight"} />
+      </div>
+    );
+  }
 }
